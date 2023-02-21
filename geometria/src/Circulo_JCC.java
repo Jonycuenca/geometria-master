9de8294f59bfe10d46cc1dc64c0a9896809a12f9
Jonattan <jcuencacerdan@gmail.com>
2023-02-21 17:19:06 +0100

    versión refactorizada

--- geometria/src/Circulo_JCC.java
+++ geometria/src/Circulo_JCC.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_JCC extends FiguraGeometrica_JCC {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_JCC(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
--- geometria/src/FiguraGeometrica_JCC.java
+++ geometria/src/FiguraGeometrica_JCC.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_JCC {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_JCC(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
